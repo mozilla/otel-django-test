d98d2cedfbe9431a21ad076249ea27de50dc0619
Basma1912 <basad@mozilla.com>
2025-10-24 09:37:03 -0400

    add __lbheartbeat__/
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("__lbheartbeat__/", views.lbheartbeat),
+    path("__lbheartbeat__", views.lbheartbeat),
     path('add/', views.add_item, name='add_item'),
     path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
