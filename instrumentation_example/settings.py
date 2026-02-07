7272866c3581d42adc4dfe6aa27616cbff1fea0c
Basma1912 <basad@mozilla.com>
2026-02-06 22:11:09 -0500

    update settings
    Signed-off-by: Basma1912 <basad@mozilla.com>

--- instrumentation_example/settings.py
+++ instrumentation_example/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = True
 
-#ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 #DEBUG = os.environ.get("DJANGO_DEBUG")
